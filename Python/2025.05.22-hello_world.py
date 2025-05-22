e788cb506cd8ad506970e5d9c209fbeb55ac772f
Klaudia Huk <klaudiahuk@gmail.com>
2025-05-22 12:39:17 +0100

    Create 2025.05.22-hello_world.py

---
+++ Python/2025.05.22-hello_world.py
@@ -0,0 +1,53 @@
+## Hello World (Following course on Udemy)
+
+### Step 1: Using Sublime Text  
+We wrote our first Python code in Sublime Text:
+
+```python
+print('hello world')
+
+We saved the file on the desktop as myexample.py so the system knows it's Python code.
+
+### Step 2: Running the code via CMD
+
+In Command Prompt:
+
+cd Desktop  
+python myexample.py
+
+The terminal responded with hello world.
+
+Alternative method (less recommended):
+
+You can also open Python directly in CMD:
+
+python  
+>>> print('hello world')  
+>>> print('hello')
+
+But this method is not recommended, because you can only write and run one line at a time.
+
+To exit Python in CMD:
+
+quit()
+
+### Step 3: Using Jupyter Notebook (Anaconda)
+
+We opened myexample.py from the Desktop using Jupyter Notebook.
+You can also start a new notebook by choosing:
+
+Jupyter → New → Python 3
+
+There, we can write:
+
+print('hello world')  
+print('hello')  
+print('new')
+
+To run the code:
+Click "Run" or press Shift + Enter.
+
+
+---
+
+And that’s our first "Hello World" in Python!
